f319b187532a08f2d232ade4a1235f974957d271
Ed <lookatemail@gmail.com>
2021-10-19 16:08:29 +0100

    Able to login to tinder account

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 FB_EMAIL = "lookatemail@gmail.com"
@@ -26,3 +26,27 @@ sleep(1)
 fb_login = driver.find_element(By.XPATH, "//span[contains(text(),'Login with Facebook')]")
 fb_login.click()
 
+#Switch to Facebook login window
+sleep(1)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.window_handles) # array of available browser windows
+print(driver.title)
+
+# accepting cookies pop up
+sleep(1)
+accept_cookies = driver.find_element(By.XPATH, "//button[contains(text(),'Allow All Cookies')]")
+accept_cookies.click()
+
+# Login and hit enter
+sleep(1)
+email = driver.find_element(By.XPATH, "//*[contains(@name,'email')]")
+password = driver.find_element(By.XPATH, "//*[contains(@name,'pass')]")
+email.send_keys(FB_EMAIL)
+password.send_keys(FB_PASSWORD)
+password.send_keys(Keys.ENTER)
+
+# Switch back to Tinder window
+driver.switch_to.window(base_window)
+print(driver.title)

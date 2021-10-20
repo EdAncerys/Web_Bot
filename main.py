fc24212098623d6ba623e845c748c03f75dfa44f
Ed <lookatemail@gmail.com>
2021-10-20 08:52:29 +0100

    Able to automate swiping task - add exeption error hadling

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, StaleElementReferenceException
 
 FB_EMAIL = "lookatemail@gmail.com"
 FB_PASSWORD = "rybzij-hyvzyd-7mArbe"
@@ -31,7 +34,7 @@ sleep(1)
 base_window = driver.window_handles[0]
 fb_login_window = driver.window_handles[1]
 driver.switch_to.window(fb_login_window)
-print(driver.window_handles) # array of available browser windows
+# print(driver.window_handles) # array of available browser windows
 print(driver.title)
 
 # accepting cookies pop up
@@ -50,3 +53,78 @@ password.send_keys(Keys.ENTER)
 # Switch back to Tinder window
 driver.switch_to.window(base_window)
 print(driver.title)
+
+# # Delay by 5 seconds to allow page to load.
+# sleep(5)
+
+# Allow location
+allow_location_button = False
+while not allow_location_button:
+    try:
+        button = driver.find_element(By.XPATH, "//span[contains(text(),'Allow')]")
+        allow_location_button = button
+    except NoSuchElementException:
+        print('allow_location_button not found')
+        sleep(2)
+
+allow_location_button.click()
+
+# Disallow notifications
+notifications_button = driver.find_element(By.XPATH, "//span[contains(text(),'Enable')]")
+notifications_button.click()
+
+# Allow Chrome notifications
+button_array = False
+while not button_array:
+    try:
+        like_button = driver.find_elements(By.XPATH, "//*[name()='svg'][@class='Scale(.5) Expand']")
+        button_array = like_button
+    except NoSuchElementException:
+        print('button_array not found')
+        sleep(2)
+
+# Like button in array of elements
+like_button = button_array[1]
+# Run loop while True
+swipe_left = True
+total_likes = 0
+
+#Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
+while swipe_left:
+    # Add a 1 second delay between likes.
+    sleep(0.5)
+    try:
+        like_button.click()
+        total_likes += 1
+        print('Total likes: ' + str(total_likes))
+    # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
+    except (ElementClickInterceptedException, StaleElementReferenceException):
+        try:
+            # match_popup = driver.find_element(By.CSS_SELECTOR, "span[class='D(f) W(100%) CenterAlign']")
+            # match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup = driver.find_element(By.XPATH, "//span[contains(text(),'Keep swiping')]")
+            match_popup.click()
+        # Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
+        except NoSuchElementException:
+            try:
+                popup = driver.find_element(By.XPATH, "//span[contains(text(),'No Thanks')]")
+                popup.click()
+            except NoSuchElementException:
+                try:
+                    popup = driver.find_element(By.XPATH, "//span[contains(text(),'Not interested')]")
+                    popup.click()
+                except NoSuchElementException:
+                    try:
+                        sleep(2)
+                        like_button.click()
+                        total_likes += 1
+                        print('Total likes: ' + str(total_likes))
+                    except NoSuchElementException:
+                        try:
+                            popup = driver.find_element(By.XPATH, "//span[contains(text(),'Discovery settings')]")
+                            print('No more matches in the area error. Total likes: ' + str(total_likes))
+                            driver.quit()
+                        except NoSuchElementException:
+                            sleep(2)
+
+# driver.quit()
